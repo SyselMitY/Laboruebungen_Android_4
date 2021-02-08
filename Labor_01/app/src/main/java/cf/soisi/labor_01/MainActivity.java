cb78f1866550b35d0290e102777d843ba0cbe48d
Peter Sysel <peter@sysel.at>
2021-02-08 09:44:58 +0100

    App finished

--- Labor_01/app/src/main/java/cf/soisi/labor_01/MainActivity.java
+++ Labor_01/app/src/main/java/cf/soisi/labor_01/MainActivity.java
@@ -2,13 +2,27 @@ package cf.soisi.labor_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "cf.sossi.labor_01.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
