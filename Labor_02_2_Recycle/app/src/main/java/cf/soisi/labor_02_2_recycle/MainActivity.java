b66b141b2576384ed21ac42ef4341fb9984d43b6
Peter Sysel <peter@sysel.at>
2021-02-15 10:51:27 +0100

    Labor 02 part 2 initial commit

---
+++ Labor_02_2_Recycle/app/src/main/java/cf/soisi/labor_02_2_recycle/MainActivity.java
@@ -0,0 +1,14 @@
+package cf.soisi.labor_02_2_recycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
